ed7e74d977e27e608e47a688e6593b7d01b581d2
Bach Vo Nguyen Duy <16770@student.vgu.edu.vn>
2023-05-18 13:51:37 +0700

    updte connection

--- backend/connection.js
+++ backend/connection.js
@@ -1,5 +1,7 @@
 const Pool = require('pg').Pool;
 
+// const user = process.env.PSQL_USER
+// console.log("user ", user)
 // const pool = new Pool({
 //     user: "postgres",
 //     password: "123456789",
@@ -10,14 +12,19 @@ const Pool = require('pg').Pool;
 //     connectionTimeoutMillis: 2000,
 // });
 
+// const pool = new Pool({
+//     user: "risen_database_user",
+//     password: "iMykYxORePmtt06mcuSkJBnoje1Nb2Ue",
+//     host: "dpg-chicdq5269vf5qd5q5dg-a.singapore-postgres.render.com",
+//     port: 5432,
+//     database: "risen_database",
+//     idleTimeoutMillis: 30000,
+//     connectionTimeoutMillis: 2000
+// });
+
+const connectionString = "postgres://risen_database_user:iMykYxORePmtt06mcuSkJBnoje1Nb2Ue@dpg-chicdq5269vf5qd5q5dg-a/risen_database"
 const pool = new Pool({
-    user: "risen_database_user",
-    password: "iMykYxORePmtt06mcuSkJBnoje1Nb2Ue",
-    host: "dpg-chicdq5269vf5qd5q5dg-a.singapore-postgres.render.com",
-    port: 5432,
-    database: "risen_database",
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000
-});
+    connectionString,
+})
 
 module.exports = pool;
