f6dbfb672690687e0e0261dbf70b4d301ed4a090
Bach Vo Nguyen Duy <16770@student.vgu.edu.vn>
2023-05-18 14:07:35 +0700

    retry connection

--- backend/connection.js
+++ backend/connection.js
@@ -24,19 +24,7 @@ const Pool = require('pg').Pool;
 
 const connectionString = "postgres://risen_database_user:iMykYxORePmtt06mcuSkJBnoje1Nb2Ue@dpg-chicdq5269vf5qd5q5dg-a/risen_database"
 const pool = new Pool({
-<<<<<<< HEAD
     connectionString,
 })
-=======
-    user: "risen_database_user",
-    password: "iMykYxORePmtt06mcuSkJBnoje1Nb2Ue",
-    host: "dpg-chicdq5269vf5qd5q5dg-a.singapore-postgres.render.com",
-    port: 5432,
-    database: "risen_database",
-    ssl: true,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000
-});
->>>>>>> 3967be30787394806cf9aba511d689a4c0424210
 
 module.exports = pool;
