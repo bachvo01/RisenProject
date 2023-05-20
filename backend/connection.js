cbf1936b92c8b67a4c5fcbb445516cc74b23d197
Bach Vo Nguyen Duy <16770@student.vgu.edu.vn>
2023-05-20 11:18:39 +0700

    backend update

--- backend/connection.js
+++ backend/connection.js
@@ -2,15 +2,15 @@ const Pool = require('pg').Pool;
 
 // const user = process.env.PSQL_USER
 // console.log("user ", user)
-// const pool = new Pool({
-//     user: "postgres",
-//     password: "123456789",
-//     host: process.env.DB_HOST,
-//     port: 5432,
-//     database: "Risen",
-//     idleTimeoutMillis: 30000,
-//     connectionTimeoutMillis: 2000,
-// });
+const pool = new Pool({
+    user: "postgres",
+    password: "123456789",
+    host: process.env.DB_HOST,
+    port: 5432,
+    database: "Risen",
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+});
 
 // const pool = new Pool({
 //     user: "risen_database_user",
@@ -22,9 +22,9 @@ const Pool = require('pg').Pool;
 //     connectionTimeoutMillis: 2000
 // });
 
-const connectionString = "postgres://risen_database_user:iMykYxORePmtt06mcuSkJBnoje1Nb2Ue@dpg-chicdq5269vf5qd5q5dg-a/risen_database"
-const pool = new Pool({
-    connectionString,
-})
+// const connectionString = "postgres://risen_database_user:iMykYxORePmtt06mcuSkJBnoje1Nb2Ue@dpg-chicdq5269vf5qd5q5dg-a/risen_database"
+// const pool = new Pool({
+//     connectionString,
+// })
 
 module.exports = pool;
